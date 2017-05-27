8acee7f089ba6be2fe16e8a9882cd2cc45ab6530
plliao <r02921027@ntu.edu.tw>
2017-05-27 10:33:19 +0800

    temp

--- DynamicProgramming/MatrixChainMultiplication/matrixChainMultiplication.cpp
+++ DynamicProgramming/MatrixChainMultiplication/matrixChainMultiplication.cpp
@@ -1,51 +1,58 @@
-#include <utility>
-#include <vector>
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
 #include <limits>
+#include "matrixChainMultiplication.h"
 using namespace std;
 
-typedef pair<int, int> matrix;
-
-void printOrder(const int* const, int, int, size_t);
-
-int matrixChainMultiplication(const vector<matrix>& matrixChain) {
-   size_t matrixNum = matrixChain.size();
-   size_t matrixDimension = matrixNum * matrixNum * sizeof(int);
-   int* costMatrix = (int*) malloc(matrixDimension);
-   int* indexMatrix = (int*) malloc(matrixDimension);
+MatrixChainMultiplication(size_t N) {
+   _matrixSpace = N * N * sizeof(int);
+   _costMatrix = (int*) malloc(matrixSpace);
+   _indexMatrix = (int*) malloc(matrixSpace);
    if (!costMatrix || !indexMatrix ) {
       cerr<<"Memory allocation error"<<endl;
+      if (_costMatrix) free(_costMatrix);
+      if (_indexMatrix) free(_indexMatrix);
       return 0;
    }
+}
 
-   memset(costMatrix, 0, matrixDimension);
-   memset(indexMatrix, 0, matrixDimension);
+~MatrixChainMultiplication() {
+   free(_costMatrix);
+   free(_indexMatrix);
+}
+
+int MatrixChainMultiplication::matrixChainMultiplication (
+   const matrix* const matrixChain,
+   size_t matrixNum
+) {
+   _maxtrixNum = matrixNum;
+   memset(costMatrix, 0, matrixSpace);
+   memset(indexMatrix, 0, matrixSpace);
   
-   for (size_t i=0; i < matrixNum; i++) {
+   for (size_t i=0; i < _matrixNum; i++) {
       size_t x = 0, y = i;
-      for (y; y < matrixNum; x++, y++) {
-         size_t index = x * matrixNum + y;
-         if (x==y) costMatrix[index] = 0;
+      for (y; y < _matrixNum; x++, y++) {
+         size_t index = x * _matrixNum + y;
+         if (x==y) _costMatrix[index] = 0;
          else if ((x+1)==y) {
-            costMatrix[index] = matrixChain[x].first * matrixChain[x].second * matrixChain[y].second;
+            _costMatrix[index] = matrixChain[x].first * matrixChain[x].second * matrixChain[y].second;
          }
          else {
             int min = numeric_limits<int>::max();
             for (int s=x; s<y; s++) {
                int cost = 0;
                if (x==s) cost = matrixChain[x].first * matrixChain[x].second * \
-                                matrixChain[y].second + costMatrix[index + matrixNum];
+                                matrixChain[y].second + _costMatrix[index + _matrixNum];
                else if (s==y-1) cost = matrixChain[x].first * matrixChain[y].first * \
-                                matrixChain[y].second + costMatrix[index - 1];
+                                matrixChain[y].second + _costMatrix[index - 1];
                else cost = matrixChain[x].first * matrixChain[s].second * \
-                           matrixChain[y].second + costMatrix[index - y + s] + \
-                           costMatrix[index + (s + 1 - x) * matrixNum];
+                           matrixChain[y].second + _costMatrix[index - y + s] + \
+                           _costMatrix[index + (s + 1 - x) * _matrixNum];
                if (cost < min) {
-                  costMatrix[index] = cost;
-                  indexMatrix[index] = s;
+                  _costMatrix[index] = cost;
+                  _indexMatrix[index] = s;
                   min = cost;
                }               
             }
@@ -57,8 +64,6 @@ int matrixChainMultiplication(const vector<matrix>& matrixChain) {
    cout<<endl;
    
    int minimumCost = costMatrix[matrixNum - 1];
-   free(costMatrix);
-   free(indexMatrix);
 
    return minimumCost;
 }
